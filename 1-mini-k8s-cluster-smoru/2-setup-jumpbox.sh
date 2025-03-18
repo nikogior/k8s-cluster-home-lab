84a89a6e1bfb103c7b1703fdbb32ef8c81358ba6
nikogior <nikogior@hotmail.com>
2025-03-18 23:56:10 +0200

    few jumpbox installation changes

--- 1-mini-k8s-cluster-smoru/2-setup-jumpbox.sh
+++ 1-mini-k8s-cluster-smoru/2-setup-jumpbox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # FROM: https://github.com/kelseyhightower/kubernetes-the-hard-way/blob/master/docs/02-jumpbox.md
 
-_remote="jumpbox"
+_remote="$JUMPBOX_IP"
 _user="root"
  
 echo "Local system name: $HOSTNAME"
@@ -14,12 +14,12 @@ echo
 # All commands should be executed using the root user under the jumpbox for convenience
 # ssh -i ~/.ssh/multipass-ssh-key root@$JUMPBOX_IP 
 
-ssh -T $_remote <<'EOL'
-	now="$(date)"
-	name="$HOSTNAME"
-	up="$(uptime)"
-	echo "Server name is $name"
-	echo "Server date and time is $now"
-	echo "Server uptime: $up"
-	echo "Bye"
+ssh -i ~/.ssh/multipass-ssh-key $_user@$_remote -T <<'EOL'
+now="$(date)"
+name="$HOSTNAME"
+up="$(uptime)"
+echo "Server name is $name"
+echo "Server date and time is $now"
+echo "Server uptime: $up"
+echo "Bye"
 EOL
